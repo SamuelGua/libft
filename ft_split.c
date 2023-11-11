cb02a3b9e7c2acffa3f15e5cb24e294767ea7d67
SamuelGua <Samuel.cely@hotmail.fr>
2023-11-11 17:25:45 +0100

    split2

--- ft_split.c
+++ ft_split.c
@@ -1,78 +1,104 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
+/*   ft_split copie.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: scely <scely@student.42.fr>                +#+  +:+       +#+        */
+/*   By: samuelcely <samuelcely@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 11:42:55 by scely             #+#    #+#             */
-/*   Updated: 2023/11/09 12:51:10 by scely            ###   ########.fr       */
+/*   Updated: 2023/11/10 23:33:48 by samuelcely       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_strlen(const char *s)
+int    ft_strlen_split(const char *s, char c)
 {
-	int	i;
+    int    i;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
+    i = 0;
+    while (s[i] && s[i] != c)
+        i++;
+    return (i);
+}
+
+int     count_words(char const *s, char c)
+{
+    int i;
+    int nb_words;
+
+    i = 0;
+    nb_words = 0;
+    if (s[0] != c)
+        nb_words++;
+    while(s[i])
+    {
+        if ((s[i] == c && s[i + 1] != c) && s[i + 1] != '\0')
+            nb_words++;
+        i++;
+    }
+    return (nb_words);
 }
 
 char **ft_split(char const *s, char c)
 {
-    int     i;
-    int     j;
-    int     k;
-    char    **new;
+    char **new;
+    int i;
+    int k;
+    int j;
 
-    i = 0;
     j = 0;
-    // creation du lv1
-    while(s[i] != c)
-    {
-        if (s[i] != c)
-            i++;
-    }
-    new = malloc(sizeof(char) * i);
+    k = 0;
+// Partie 1 Malloc lv 1
+    i = count_words(s, c);
+    new = malloc(sizeof(char *) * (i + 1));
     if (!new)
         return (0);
-    //creation du lv2
+    new[i] = (char *)'\0';
+    //new[i] = (char*)'\0'; 
+     // Ne pas oublier de mettre le nul a la fin. lv 2 surement
+// Partie 2 Malloc lv 2
     i = 0;
     while(s[i])
     {
         k = 0;
-        while(s[i] != c)
+        if (s[i] != c && s[i])
             {
-                i++;
-                k++;
-            }
-        new[i][j] = malloc(sizeof(char) * k + 1);
-        if (!new)
-            return (0);
-        j++;
+                k = ft_strlen_split(&s[i], c);
+                new[j++] = malloc(sizeof(char) * k + 1);
+                new[j - 1][k] = '\0';
+                if (!new)
+                    return NULL;
+                i += k;
+            } 
+            else
+            i++;
     }
-    // faire la copie
-
-    
-
-
+// Partie 3 Copier coller
+    j = 0;
+    i = 0;
+    while (s[i])
+       {
+        if (s[i] == c)
+            i++;
+        k = 0;
+        while (s[i] != c && s[i])
+        {
+            new[j][k++] = s[i];
+            i++;
+            if (s[i] == c)
+                j++;
+        }
+       }
+      
+       return(new);
 }
-
 int main()
 {
-    char *d = "J'arrive,Demain,Bonjour";
-    char **c = ft_split(d, ',');
-    split(d,)
-
-    int i = 0;
-    while(*c[i])
-    {
-        printf("%s", c[i]);
-    }
-
+    char *d = "ORIGAMI";
+    char **new = ft_split(d, '.');
+    // char **c = ft_split(d, ',');
+    printf("%s", new[0]);
+    //free(new);
 }
