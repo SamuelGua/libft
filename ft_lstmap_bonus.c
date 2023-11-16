c18a6c4b8f40c31c33e2c87dc9df702030058a25
Samuel Cely <scely@bess-f3r3s11.clusters.42paris.fr>
2023-11-16 12:52:58 +0100

    fin

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,46 +6,31 @@
 /*   By: scely <scely@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 10:28:36 by scely             #+#    #+#             */
-/*   Updated: 2023/11/16 12:12:12 by scely            ###   ########.fr       */
+/*   Updated: 2023/11/16 12:52:36 by scely            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*=================================*/
-// int ft_lstsize(t_list *lst)
-// {
-//     int i;
-
-//     i = 0;
-//     while (lst != NULL)
-//     {
-//         i++;
-//         lst = lst->next; 
-//     }
-//     return (i);
-// }
-/*=================================*/
-
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    int i;
-    t_list *lstbis;
-    t_list *temp;
+	int		i;
+	t_list	*lstbis;
+	t_list	*temp;
 
-    i = ft_lstsize(lst);
-    lstbis = malloc(sizeof(lst) * i);
-    temp = malloc(sizeof(lst));
-    if (!del)
-        return (lst);
-    while (lst != NULL)
-    {
-        temp->content = ft_lstnew(f(lst));
-        if (temp->content == NULL)
-            del(lstbis->content);
-        lstbis->content = temp->content;
-        lst = lst->next;
-        lstbis = lstbis->next;   
-    }
-    return (lst);
+	i = ft_lstsize(lst);
+	lstbis = malloc(sizeof(lst) * i);
+	temp = malloc(sizeof(lst));
+	if (!del)
+		return (lst);
+	while (lst != NULL)
+	{
+		temp->content = ft_lstnew(f(lst));
+		if (temp->content == NULL)
+			del(lstbis->content);
+		lstbis->content = temp->content;
+		lst = lst->next;
+		lstbis = lstbis->next;
+	}
+	return (lst);
 }
